00283c1054e0098dcb094a2a9537738ce7d7fa59
Malachi Burke <malachi.burke@gmail.com>
2020-10-11 11:53:05 -0700

    Compiles, does not yet run

--- src/utility-belt/main.cpp
+++ src/utility-belt/main.cpp
@@ -1,6 +1,8 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include <entt/entt.hpp>
+
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
