35830a0f354e1dffbe61d059ff62c0f53e4de380
Malachi Burke <malachi.burke@gmail.com>
2020-10-11 20:50:51 -0700

    Trying out ENTT

--- src/utility-belt/main.cpp
+++ src/utility-belt/main.cpp
@@ -3,11 +3,15 @@
 
 #include <entt/entt.hpp>
 
+int entt_test();
+
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
+
+    entt_test();
 
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
